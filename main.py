305894e1b07602230279d7a7fd48c44417b482d9
Kunqi Zhao <110202952+ZhaoKunqi@users.noreply.github.com>
2023-06-28 02:32:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-debug_mode=0
+debug_mode=1
 
 from time import sleep,ctime
-from refresh import refresh_ip
 import sys,os
 import requests
 my_address=""
 is_error=0
+
+def refresh_ip():
+    import requests
+    import json
+    debug=0
+    url="https://dnsapi.cn/Record.Ddns"
+    parm={
+        "login_token":"你的DNSPod API令牌",
+        "format":"json",
+        "domain_id":"你的DNSPod域名ID",
+        "record_id":"你的DNSPod域名下那条解析的ID",
+        "sub_domain":"自定义主机名",
+        "record_line":"默认",
+    }
+    json_recv=requests.post(url=url,data=parm).content.decode()
+    result=json.loads(json_recv)
+    if debug_mode:print(json.dumps(result))
+
 while True:
     try:
-        current_address=str(requests.get(url="https://myip.wtf/text").text)
+        requests.packages.urllib3.util.connection.HAS_IPV6 = False
+        current_address=str(requests.get(url="https://wtfismyip.com/text").text)
         is_error=0
     except:
         is_error=1
@@ -31,4 +49,4 @@ while True:
     elif my_address == current_address:
         if debug_mode:
             sys.stdout.write(ctime()+":[Normal]Status Normal,IP not change.\n")
-    sleep(60)
+    sleep(3600)
